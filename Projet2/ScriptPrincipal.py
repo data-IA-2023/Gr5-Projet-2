bb0b5c43b8a427b4e111430053470d0b3886a5dd
Laamh <clement.nogues.pro@gmail.com>
2024-01-22 19:09:33 +0100

    "big update"

--- Projet2/ScriptPrincipal.py
+++ Projet2/ScriptPrincipal.py
@@ -1,34 +1,63 @@
 from imports import *
 
 path = "https://raw.githubusercontent.com/remijul/dataset/master/SMSSpamCollection"
-data = pd.read_csv(path, names = ["flag", "text"], sep='\t')
+data = pd.read_csv(path, names = ["Flag", "text"], sep='\t')
 
 preprocess(data)
 
+data["LEN"] = data["text"].str.split(r"(?!^)").str.len()
+data["NB"] = data["text"].str.split().str.len()
+
+preparation = ColumnTransformer(
+    transformers=[
+        ('data_cat', CountVectorizer(), "text"),
+        ('data_num', RobustScaler(), ["LEN","NB"])
+])
+
 pipel = Pipeline(steps=[
-                        ('vectorisation', CountVectorizer()),
-                        ('pcaisation', TruncatedSVD(n_components=200)),
-                        ('modelisation', LogisticRegression())
+                        ('vector/Scale', preparation),
+                        ('modelisation', DecisionTreeClassifier())
                         ])
 
 # Features
-X = data["text"]
+X = data.drop("Flag", axis = 1, inplace=False)
 # Target
-y = data['flag']
+y = data['Flag']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state= 3)
+#Z = input()
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 pipel.fit(X_train,y_train)
-
 y_pred = pipel.predict(X_test)
-print("Prediction with Count Vectorizer et Logistic Regression:", accuracy_score(y_test, y_pred))
+print("Prediction with Count Vectorizer et Logistic Regression:", recall_score(y_test, y_pred, pos_label='spam'))
+
+def find(x):
+    if x == 1:
+        print ("Message is SPAM")
+    else:
+        print ("Message is NOT Spam")
 
 
 
 
+#print("Prediction with Count Vectorizer et Logistic Regression:", recall_score(y_test, y_pred, pos_label='spam'))
 
+#print(confusion_matrix(y_test, y_pred))
 
+#lb_encod = LabelEncoder()
+#cm_plot = ConfusionMatrixDisplay(confusion_matrix,
+#                                display_labels=np.unique(lb_encod.inverse_transform(model.classes_)))
 
+#cm_plot.plot()
+"""
+TP = sum((y_test == 'spam') & (y_pred == 'spam'))
+print(TP)
+
+FN = sum((y_test == 'spam') & (y_pred == 'ham'))
+print(FN)
+
+print(TP / (TP + FN))"""
 
 
 # Test TFIDF et KNN | TFIDF et Logistic regression | CountVectorizer et KNN. Moins précis
--- Projet2/imports.py
+++ Projet2/imports.py
@@ -8,3 +8,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import streamlit as st
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, recall_score
+from sklearn.preprocessing import LabelEncoder, RobustScaler
+from sklearn.naive_bayes import CategoricalNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.compose import ColumnTransformer
+
+
+def preprocess(a):
+    a.drop_duplicates(inplace=True)
+    a.dropna(inplace=True)
