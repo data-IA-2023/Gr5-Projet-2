d9a1ba26f8df0d85a77e29165fe74d8ea4586af2
ApologosPM <paperazam@gmail.com>
2024-01-22 16:55:49 +0100

    work

--- Projet 2/Streamlit_test.py
+++ Projet 2/Streamlit_test.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import streamlit as st
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import recall_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import RobustScaler
 
 path = "https://raw.githubusercontent.com/remijul/dataset/master/SMSSpamCollection"
 data = pd.read_csv(path, names = ["flag", "text"], sep='\t')
@@ -26,3 +28,34 @@ with st.sidebar:
     )
 
 st.write(datasl[datasl["flag"] == add_radio].head(nom_mess))
+
+# preprocessing
+path = "https://raw.githubusercontent.com/remijul/dataset/master/SMSSpamCollection"
+data = pd.read_csv(path, names = ["flag", "text"], sep='\t')
+data["len"] = data["text"].str.split(r"(?!^)").str.len()
+data["nb"] = data["text"].str.split().str.len()
+data.dropna(inplace= True)
+data.drop_duplicates(inplace = True)
+
+# agroupement de encodage et modelisation
+
+preparation = ColumnTransformer(
+    transformers=[
+        ('data_cat', CountVectorizer() , "text"),
+        ('data_num', RobustScaler(), ["len","nb"])
+    ])
+
+pipe = Pipeline(steps=[
+                       ('preparation', preparation),
+                       ('modelisation', DecisionTreeClassifier())
+                       ])
+
+# diviser train et test, appel du pipeline et regarder la performance
+
+X = data.drop(["flag"], axis = 1)
+y = data["flag"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state= 3)
+pipe.fit(X_train,y_train)
+y_pred = pipe.predict(X_test)
+print("Score avec tree classifier et count vectorizer", recall_score(y_test, y_pred, pos_label="spam"))
+
