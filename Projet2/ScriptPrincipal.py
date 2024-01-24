f1ac67a1d559a9d8133ee8abc88ee42fefefb3c6
Laamh <clement.nogues.pro@gmail.com>
2024-01-24 18:57:29 +0100

    "update"

--- Projet2/ScriptPrincipal.py
+++ Projet2/ScriptPrincipal.py
@@ -1,12 +1,24 @@
 from imports import *
 
+# Importation CSV
 path = "https://raw.githubusercontent.com/remijul/dataset/master/SMSSpamCollection"
 data = pd.read_csv(path, names = ["Flag", "text"], sep='\t')
 
+# Preprocessing, voir fonction dans "imports"
 preprocess(data)
 
+# Création de deux nouvelles colonnes, longueur des phrases et nombre de mots
 data["LEN"] = data["text"].str.split(r"(?!^)").str.len()
 data["NB"] = data["text"].str.split().str.len()
+
+sm = SMOTENC(categorical_features = ['text'], random_state= 3)
+
+# Features
+X = data.drop("Flag", axis = 1, inplace=False)
+# Target
+y = data['Flag']
+# Train Test Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 preparation = ColumnTransformer(
     transformers=[
@@ -18,25 +30,28 @@ pipel = Pipeline(steps=[
                         ('vector/Scale', preparation),
                         ('modelisation', DecisionTreeClassifier())
                         ])
+                        
+params = {
+    'modelisation': [LogisticRegression(), KNeighborsClassifier(), DecisionTreeClassifier()],
+}
 
-# Features
-X = data.drop("Flag", axis = 1, inplace=False)
-# Target
-y = data['Flag']
+scorer = {'accuracy': 'accuracy', 'recall': make_scorer(recall_score, pos_label='spam')}
+searchCV = GridSearchCV(pipel, params, scoring=scorer, refit='recall')
 
-#Z = input()
-# Train test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-pipel.fit(X_train,y_train)
-y_pred = pipel.predict(X_test)
-print("Prediction with Count Vectorizer et Logistic Regression:", recall_score(y_test, y_pred, pos_label='spam'))
+searchCV.fit(X_train,y_train)
+y_pred = searchCV.predict(X_test)
 
-def find(x):
+print("Prediction with Count Vectorizer and the best model:", searchCV.best_score_)
+
+#print("Prediction with Count Vectorizer et Logistic Regression:", recall_score(y_test, y_pred, pos_label='spam'))
+
+
+"""def find(x):
     if x == 1:
         print ("Message is SPAM")
     else:
-        print ("Message is NOT Spam")
+        print ("Message is NOT Spam")"""
 
 
 
--- Projet2/imports.py
+++ Projet2/imports.py
@@ -4,18 +4,17 @@ from nltk.corpus import stopwords
 import re
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
 import streamlit as st
 from sklearn.pipeline import Pipeline
 from sklearn.decomposition import TruncatedSVD
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, recall_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, recall_score, make_scorer, accuracy_score
 from sklearn.preprocessing import LabelEncoder, RobustScaler
 from sklearn.naive_bayes import CategoricalNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.compose import ColumnTransformer
-
+from imblearn.over_sampling import SMOTENC
 
 def preprocess(a):
     a.drop_duplicates(inplace=True)
